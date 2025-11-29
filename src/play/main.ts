c9e970ac5f15837d89bda54692db63105ae3b4a6
Danny V <dimkinv@gmail.com>
2025-11-29 23:48:15 +0200

    remove unused test wrapper example from main.ts

--- src/play/main.ts
+++
@@ -1,8 +0,0 @@
-import * as test from '../lib/test';
-
-// Example runtime: iterate over all creeps and run the simple wrapper example.
-for (const name in Game.creeps) {
-	const creep = Game.creeps[name];
-	if (!creep) continue;
-	test.runWrapperExample(creep);
-}
